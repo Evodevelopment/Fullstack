07f76f277e704a5c6a01d6b6cd8a5a7a5e209e28
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-07-28 06:28:43 +0200

    Create jsstack.js

---
+++ jsstack.js
@@ -0,0 +1,6 @@
+if (skills.includes("HTML, CSS a JavaScript ")) {
+   alert("join us");
+}
+
+#skills is expected to be an array of strings (e.g., ["HTML", "CSS", "JavaScript"]).
+#The includes() method is called on the skills array to check if it contains the specific string "HTML, CSS a JavaScript ".
