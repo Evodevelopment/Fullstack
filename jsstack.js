765a0daf9b6070fef7f451111c861670e1d50c6d
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-07-28 06:30:00 +0200

    Update jsstack.js

--- jsstack.js
+++ jsstack.js
@@ -2,7 +2,8 @@ if (skills.includes("HTML, CSS a JavaScript ")) {
    alert("join us");
 }
 
-/*skills is expected to be an array of strings (e.g., ["HTML", "CSS", "JavaScript"]).
+/*
+skills is expected to be an array of strings (e.g., ["HTML", "CSS", "JavaScript"]).
 The includes() method is called on the skills array to check if it contains the specific string "HTML, CSS a JavaScript ".
 
 */
